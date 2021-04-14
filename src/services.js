1ef4d3dc07e39ff33cbad4839aaa195691f07707
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-04-14 21:15:06 +0500

    background color updated

--- src/services.js
+++ src/services.js
@@ -9,7 +9,7 @@ function Services(props){
                 <div class="w3-third"><h2 class="w3-center w3-red">{props.offer3}</h2></div>
             </div>
             <div class="w3-row">
-                <div class="w3-third"><h2 class="w3-center w3-orange">{props.offer4}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-brown">{props.offer4}</h2></div>
                 <div class="w3-third"><h2 class="w3-center w3-purple">{props.offer5}</h2></div>
                 <div class="w3-third"><h2 class="w3-center w3-teal">{props.offer6}</h2></div>
             </div>
