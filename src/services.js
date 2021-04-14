98a7fa309b3f7fb7e337e934e07f5de34df3d8bb
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-04-14 21:16:14 +0500

    style update

--- src/services.js
+++ src/services.js
@@ -1,17 +1,17 @@
 import React from 'react' 
 
 function Services(props){
-    return(
+    return( 
         <div class="w3-container">
             <div class="w3-row">
-                <div class="w3-third"><h2 class="w3-center w3-blue">{props.offer1}</h2></div>
-                <div class="w3-third"><h2 class="w3-center w3-green">{props.offer2}</h2></div>
-                <div class="w3-third"><h2 class="w3-center w3-red">{props.offer3}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-blue w3-padding-64">{props.offer1}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-green w3-padding-64">{props.offer2}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-red w3-padding-64">{props.offer3}</h2></div>
             </div>
             <div class="w3-row">
-                <div class="w3-third"><h2 class="w3-center w3-brown">{props.offer4}</h2></div>
-                <div class="w3-third"><h2 class="w3-center w3-purple">{props.offer5}</h2></div>
-                <div class="w3-third"><h2 class="w3-center w3-teal">{props.offer6}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-brown w3-padding-64">{props.offer4}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-purple w3-padding-64">{props.offer5}</h2></div>
+                <div class="w3-third"><h2 class="w3-center w3-teal w3-padding-64">{props.offer6}</h2></div>
             </div>
         </div>
         
